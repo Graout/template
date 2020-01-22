5d1be212080b391cd5331c8eaa9361e2646be449
Garout <emil.stromdahl@live.com>
2020-01-22 19:42:40 +0100

    init commit

---
+++ public/electron.js
@@ -0,0 +1,71 @@
+const { app, BrowserWindow, Menu, ipcMain, MenuItem } = require("electron");
+const path = require("path");
+const isDev = require("electron-is-dev");
+
+let win;
+const mainMenuTemplate = [];
+function createWindow() {
+    // Create the browser window.
+    win = new BrowserWindow({
+        minWidth: 1050,
+        minHeight: 600,
+        resizable: true,
+        show: false,
+        frame: true,
+        webPreferences: {
+            nodeIntegration: true,
+            webSecurity: false
+        }
+    });
+    win.once("ready-to-show", () => {
+        win.show();
+    });
+
+    win.loadURL(
+        isDev
+            ? "http://localhost:3000"
+            : `file://${path.join(__dirname, "../build/index.html")}`
+    );
+
+    win.on("closed", () => {
+        win = null;
+    });
+}
+
+app.on("ready", () => {
+    createWindow();
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    // Insert menu
+    Menu.setApplicationMenu(mainMenu);
+});
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
+
+app.on("activate", () => {
+    if (win === null) {
+        createWindow();
+    }
+});
+
+if (isDev) {
+    mainMenuTemplate.push({
+        label: "Dev Tools",
+        submenu: [
+            {
+                label: "toggle DevTools",
+                accelerator:
+                    process.platform == "darwin" ? "Command+I" : "Ctrl+I",
+                click(item, focusedWindow) {
+                    focusedWindow.toggleDevTools();
+                }
+            },
+            {
+                role: "reload"
+            }
+        ]
+    });
+}
