6c8bdbe3fdea95b94ca62249cce74eca203e0c41
Garut <emil.stromdahl@live.com>
2020-02-01 23:24:11 +0100

    changed back master

--- src/components/pages/main.jsx
+++ src/components/pages/main.jsx
@@ -1,31 +1,9 @@
 import React from "react";
-const { remote } = window.require("electron");
-const { dialog } = remote;
-
-const openimg = () => {
-    let mainWindow = remote.getCurrentWindow();
-    dialog
-        .showOpenDialog(mainWindow, {
-            properties: ["openFile"],
-            filters: [{ name: "Images", extensions: ["jpg", "png", "gif"] }]
-        })
-        .then(result => {
-            if (!result.canceled) {
-                console.log(result.filePaths[0]);
-                document.querySelector(".img").src = result.filePaths[0];
-            }
-        })
-        .catch(err => {
-            console.log(err);
-        });
-};
 
 export default function main() {
     return (
         <div className="container-main">
-            <div onClick={openimg} className="wrapper">
-                <img src="" className="img" alt="test" />
-            </div>
+            <div className="wrapper"></div>
         </div>
     );
 }
--- src/container/App.js
+++ src/container/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Main from "./components/main";
+import Main from "../components/pages/main";
 import FrameBar from "../components/frameBar";
 import "./App.css";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
