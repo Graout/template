3bbfd7794af91db9e86b9dceaae9de01925bbe65
Garut <emil.stromdahl@live.com>
2020-09-17 21:17:43 +0200

    cleaning

--- src/components/pages/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function main() {
-    return (
-        <div className="container-main">
-            <div className="wrapper"></div>
-        </div>
-    );
-}
